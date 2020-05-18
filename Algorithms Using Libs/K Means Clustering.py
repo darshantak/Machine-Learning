5f0327018ed211805bf411f45dabadabb2630cd4
darshantak <30834020+darshantak@users.noreply.github.com>
2020-05-19 00:20:46 +0530

    Used Libraries for algorithms

---
+++ Algorithms Using Libs/K Means Clustering.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 15 20:50:07 2020
+
+@author: cycle
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+from sklearn.cluster import KMeans
+
+X = np.array([[1, 2],
+              [1.5, 1.8],
+              [5, 8],
+              [8, 8],
+              [1, 0.6],
+              [9, 11]])
+clf=KMeans(n_clusters=2)
+clf.fit(X)
+
+centroids=clf.cluster_centers_
+labels=clf.labels_
+centroids
+type(centroids)
+colors=10*["g.","r.","c.","b.",]
+
+for i in range(len(X)):
+    plt.plot(X[i][0],X[i][1],colors[labels[i]],markersize=10)
+    
+plt.scatter(centroids[:,0],centroids[:,1],marker='x',s=150,linewidths=5)
+plt.show()
